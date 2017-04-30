a25f89af1547666dc3343b514a6d7b6323e0acb9
Unknown <kinzie.colleen@gmail.com>
2017-04-29 20:53:41 -0600

    Added Calculator Variables
    Int, Text, two Input Fields

--- Calculator/Assets/CalcScript.cs
+++ Calculator/Assets/CalcScript.cs
@@ -6,6 +6,11 @@ using UnityEngine.EventSystems;
 
 public class CalcScript : MonoBehaviour {
 
+	public InputField input1;
+	public InputField input2;
+	public Text result;
+	int number;
+	
 	// Use this for initialization
 	void Start () {
 		
